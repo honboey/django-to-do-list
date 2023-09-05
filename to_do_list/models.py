1c00354edf275f24d4f5e4dd34deb9be928b21ab
Hon Boey <hon@dialogicstudios.com.au>
2023-09-05 23:12:13 +1000

    Add completed boolean to Task model

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -5,4 +5,7 @@ class Task(models.Model):
 	name = models.CharField(max_length=200)
 	description = models.CharField(max_length=200)
 	deadline = models.DateTimeField()
-	
+	completed = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
