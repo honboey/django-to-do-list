9f146ed830953232b62debc7a3884abe13abb654
Hon Boey <hon@dialogicstudios.com.au>
2023-09-05 22:58:42 +1000

    Create to-do list app • Add index view for app with placeholder content • Add view to URLs • Create Task model • Make migrations and migrate

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "to_do_list.apps.ToDoListConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Australia/NSW"
 
 USE_I18N = True
 
