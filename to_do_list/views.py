6d1f97058afc3bd5f8e858231dc908b51e29f3e1
Hon Boey <hon@dialogicstudios.com.au>
2023-09-07 18:52:00 +1000

    Add task detail view

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Task
 
@@ -10,3 +10,11 @@ def index(request):
 		"to_do_list": to_do_list,
 	}
 	return render(request, "to_do_list/index.html", context)
+
+
+def task_detail(request, task_id):
+	task = get_object_or_404(Task, pk=task_id)
+	context = {
+		"task": task
+	}
+	return render(request, "to_do_list/task_detail.html", context)
